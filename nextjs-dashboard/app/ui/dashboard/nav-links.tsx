4e3764f5cdf21b4dd2bce332c7f3fe36cc611e6d
Scorhenao <samcorhenao@gmail.com>
2024-09-22 20:04:20 -0500

    change solving problems with the page menu

--- nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -18,6 +18,7 @@ const links = [
     icon: DocumentDuplicateIcon,
   },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'menu', href: '/dashboard/menu', icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
