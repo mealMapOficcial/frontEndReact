208229a33fa78ef9490a9fc853ff0f4038bde54c
alejandroava <alejandroalzate.ava@gmail.com>
2024-09-25 18:53:50 -0500

    fix login

--- nextjs-dashboard/app/ui/dashboard/sidenav.tsx
+++ nextjs-dashboard/app/ui/dashboard/sidenav.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
 import { PowerIcon } from '@heroicons/react/24/outline';
-import { signOut } from '@/auth';
 import Image from 'next/image';
 
 export default function SideNav() {
